e276561cd199375b05e2abdce48e826174223468
JoseSeguridata <86741547+JoseSeguridata@users.noreply.github.com>
2021-07-01 10:36:44 -0500

    Add files via upload

---
+++ src/components/Styles/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+    white: '#fff',
+    grey: '#828282',
+    hex:'#E9E9E9',
+    darkgrey: '#4f4f4f',
+     copri:'#002530',
+    primary: '#B80000',
+    background: '#f3f5f5',
+    discount: 'rgba(255, 0, 0, 0.6)',
+    greyColor: "#A9A9B0",
+    text: '#333333',
+    secundary:'rgb(204, 142, 18)',
+    blackColor: "#3B3B3B",
+  };
+  
+  export default colors;
+  
